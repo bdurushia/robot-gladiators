b79b9c2724aac441947e08b0218b4b92e160f802
bdurushia <b.durushia@gmail.com>
2021-10-28 08:11:51 -0500

    Fixed the typo in the window.prompt after var shopOptionPrompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -113,7 +113,7 @@ var endGame = function() {
 
 var shop = function() {
     // ask player what they'd like to do
-    var shopOptionPrompt = window/prompt(
+    var shopOptionPrompt = window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
     );
     // use switch to carry out action
