bf3c1b172abd6bb6b13f93c56b72e2a0e6f276d5
bdurushia <b.durushia@gmail.com>
2021-10-27 23:25:05 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,18 +64,63 @@ var fight = function(enemyName) {
     }
   };
 
-for(var i = 0; i < enemyNames.length; i++) {
+  // Function to start a new game
+var startGame = function() {  
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to the Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(enemyNames[i]);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    endGame();
+};
+
+var endGame = function() {
+    // if player is still alive, player wins!
     if (playerHealth > 0) {
-        window.alert("Welcome to the Robot Gladiators! Round " + (i + 1));
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(enemyNames[i]);
+        window.alert("Great job, you've survived the game! You ow have a score of " + playerMoney + ".");
     } else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        window.alert("You've lost your robot in the battle.");
+    }
+    // ask the player if they'd like to play agian
+    var playAgainConfirm = window.confirm("Would you like to play agian?");
+    if (playAgainConfirm) {
+        //restart game
+        startGame();
+    } else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 }
+
+// START GAME WHEN PAGE LOADS
+startGame();
+
+
 
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
 //      * Fight all enemy-robots
+
+/* Wrap the game logic in a startGame() function
+When the player is defeated or there are no more enemies, call an endGame() function that:
+Alerts the player's total stats
+Asks the player if they want to play again
+If yes, call startGame() to restart the game
+After the player skips or defeats an enemy (and there are still more robots to fight):
+Ask the player if they want to "shop"
+If no, continue as normal
+If yes, call the shop() function
+In the shop() function, ask player if they want to "refill" health, "upgrade" attack, or "leave" the shop
+If refill, subtract money points from player and increase health
+If upgrade, subtract money points from player and increase attack power
+If leave, alert goodbye and exit the function
+If any other invalid option, call shop() again */
